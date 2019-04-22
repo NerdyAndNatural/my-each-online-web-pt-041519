4dfe7a4c588700b41edd941aa2bf713da4c4328f
Sarah Williams <srwilliams8678@gmail.com>
2019-04-22 17:23:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,5 @@
 def my_each (array)
   i = 0
-  while i < array.length
-  yield(array[i])
-    i = i + 1
-  end
-end
-
-def hello(array)
-  i = 0
   collection = []
   while i < array.length
     collection << yield(array[i])
